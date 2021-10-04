097297f156f0bf22487bcdbbcc867ba7cc778ea2
jchen302023 <jchen30@stuy.edu>
2021-10-04 19:15:42 -0400

    initsetup

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,5 +1,10 @@
 public class Greet {
      public static void main (String [] args) {
-         System.out.println ("Oh hello again!");
+               greet("Ducky");
+               greet("Brain Maggot");
+               greet("Last Brain Cell");
+     }
+      public static void greet(String name){
+         System.out.println ("Oh hello again" + name + "!");
      }
 } 
