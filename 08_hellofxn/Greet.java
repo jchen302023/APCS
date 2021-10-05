37fc692c563aba56b9b43ab31fbd111be553e244
jchen302023 <90912786+jchen302023@users.noreply.github.com>
2021-10-04 21:05:33 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -9,9 +9,9 @@ public class Greet {
      }
 } 
 
-*/ The star thing is supposed to make comments, wow. Didn't know that.
-*/ Three names are created and they have to be greeted.  Returns three lines:
+/* The star thing is supposed to make comments, wow. Didn't know that.
+Three names are created and they have to be greeted.  Returns three lines:
 *Oh hello again Ducky!
 *Oh hello again Brain Maggot!
 *Oh hello again my last brain cell!
-
+*/
