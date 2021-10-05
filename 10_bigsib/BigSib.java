d36524f683420aa8c7760189194cd89bb57c6d53
jchen302023 <jchen30@stuy.edu>
2021-10-04 22:11:37 -0400

    initsetup

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -4,12 +4,14 @@ APCS
 HW10: Refactor Big Sib One
 10/4/21
 */
-
-public class Greet {
-    public static void main( String[] args )
-    {
-        System.out.println( BigSib.greet("Crosby") );
-        System.out.println( BigSib.greet("Stills") );
-        System.out.println( BigSib.greet("Nash") );
+public class BigSib {
+    public static void main( String[] args ) 
+   {
+        greet("Crosby");
+        greet("Stills");
+        greet("Nash");
+    }
+     public static void greet(String name){
+        System.out.println (name);
     }
 }
--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -4,11 +4,12 @@ APCS
 HW10: Refactor Big Sib One
 10/4/21
 */
+
 public class Greet {
     public static void main( String[] args )
     {
-        System.out.println(greet("Crosby") );
-        System.out.println(greet("Stills") );
-        System.out.println(greet("Nash") );
+        System.out.println(BigSib.greet("Crosby") );
+        System.out.println(BigSib.greet("Stills") );
+        System.out.println(BigSib.greet("Nash") );
     }
 }
