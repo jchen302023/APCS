b58952dfa8a7d4f56790f881869665e9c7162aa7
jchen302023 <jchen30@stuy.edu>
2021-10-05 23:59:54 -0400

    initsetup

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,31 @@
+/*
+Team Jelly: Lea Brandon Jacc (Gunter, Ducky, Large)
+APCS
+HW12: On Elder Individuality and the Elimination of Radio Fuzz
+10/5/21
+*/
+/* DISCO: Prints out freshmanfreshman. I don't know how to get it to work. Best I could
+do. Don't know how to refer back to the name from setHelloMsg so that I can use it in the greet.
+Tried putting it outside the method boxes but it just said non-static variables cannot be referenced
+in a static context. 
+QCC: Help.
+*/ 
+
+public class BigSib {
+    
+     public static String setHelloMsg(String name) 
+     {String hellomessage;
+
+      return (name); 
+     }
+
+     public static String greet(String name)
+     { String greet;
+      
+      String hellomessage = setHelloMsg(name);
+      
+      return (hellomessage + name);
+     }
+ 
+}
+       
