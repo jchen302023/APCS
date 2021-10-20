f09ff62fe4f6c3f619911f0a4e6660699d94c15e
jchen302023 <jchen30@stuy.edu>
2021-10-20 12:33:39 -0400

    wee

---
+++ 21_StatsDriver/Statsdriver.java
@@ -0,0 +1,18 @@
+
+
+
+public class StatsDriver {
+	public static void main (String [] args) {
+	StatsDriver State = new StatsDriver();
+	State.mean(6,8); // 7
+	State.max(6,8); // 8
+	State.geoMean(6,8); // 10
+	}
+	
+	/*public static int mean(int 6, int 8)
+	public static double mean(double 6, double 8)
+	public static int max(int 6, int 8)
+	public static double(
+	public static int 
+	public static double */ 
+}
