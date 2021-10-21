3b4e7cbf582bf11f613eca2ee9e5e0e00972390a
jchen302023 <jchen30@stuy.edu>
2021-10-20 21:01:13 -0400

    wee

--- 21_StatsDriver/Stats.java
+++ 21_StatsDriver/Stats.java
@@ -1,13 +1,20 @@
-/*Jacc Chen
+/*
+  Jacc Chen
   APCS
   HW21: Statistically Speaking
+  2021-10-20
+  DISCO: Java math!! Wacky. Why do data types sometimes don't have to be mentioned
+  but in other places they do, and it has to be stated in parentheses. Java's "and"
+  statement is two ands: &&. 
+  QCC: Math.sqrt built in? Saves us work. Can we use less parantheses because it 
+  is starting to get annoying? I need more efficient way. 
 */
 
 
 public class Stats {
 
   public static int mean(int a, int b) {
-  	int ab = ((a + b) / 2);
+  	int ab = ((a + b) / 2); // return ((a + b) / 2); works too
         return (ab);   
   }
 
@@ -40,19 +47,31 @@ public class Stats {
 
   }
 
-  /* public static int max(int a, int b, int c) {
-
+  public static int max(int a, int b, int c) {
+	if (a > b && a > c) {
+		return (a); }
+        else if (b > a && b > c) {
+		return (b); }
+        else {
+		return (c); }
   }
 
   public static double max(double a, double b, double c) {
-
+	if (a > b && a > c) {
+		return (double) (a); }
+        else if (b > a && b > c) {
+		return (double) (b); }
+        else {
+		return (double) (c); }
   }
 
   public static int geoMean(int a, int b, int c) {
-
+	int abc = a * b * c;
+        return (int) Math.cbrt(abc);
   }
 
   public static double geoMean(double a, double b, double c) {
-
-  } */ 
-}
+	double abc = a * b * c;
+        return (double) Math.cbrt(abc);
+  } 
+} // finish 
