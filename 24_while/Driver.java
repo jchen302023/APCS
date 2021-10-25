811181905cc60f42b9e57ee7c74a5152a2b108e9
jchen302023 <jchen30@stuy.edu>
2021-10-25 13:20:00 -0400

    wee

---
+++ 24_while/Driver.java
@@ -0,0 +1,34 @@
+/**
+APLE - Jacc Chen (Large) + Ivina Wang (Sealie)
+APCS
+HW24 -- Get It While You Can
+2021-10-25
+
+DISCO:
+- 
+
+QCC:
+- 
+*/
+
+public class Driver {
+  	public static void main( String[] args ) {
+  		public int x;
+  		public int y = flipsCtr;
+  		Coin aple = new Coin();
+ /*		
+  		if x <= headCtr {
+  			aple.flip();x += 1;
+  				While y <= 65536
+  		
+  		While x <= headCtr {
+  			aple.flip();
+  			x += 1
+  		}
+  		return y
+*/
+    
+
+  }
+  
+}
