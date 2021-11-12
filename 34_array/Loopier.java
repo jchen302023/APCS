3a9cdf8b217f23529cc91179cc20dbd3777a7afe
jchen302023 <jchen30@stuy.edu>
2021-11-12 13:11:12 -0500

    wee

---
+++ 34_array/Loopier.java
@@ -0,0 +1,18 @@
+/*
+    Jacc Chen, Anjini Katari
+    APCS
+    HW34 -- A Pirate's Life for Me
+    2021-11-12
+    DISCO:
+*/
+
+public class Loopier {
+  public static int linSearch( int[], int target) {
+
+  } // end method linSearch
+  
+  public static int linSEarchR( int[], int target) {
+
+  } // end method linSearchR
+
+} // end class Loopier
