6979e75538577a3576903241b659275aca252a39
jchen302023 <jchen30@stuy.edu>
2021-11-16 19:26:05 -0500

    wee

--- cb/array-1/firstLast6.java
+++ cb/array-1/firstLast6.java
@@ -21,6 +21,6 @@ public static void main (String[] args) {
   System.out.println(firstLast6(new int[] {1, 2, 6}));
   System.out.println(firstLast6(new int[] {6, 1, 2, 3}));
   System.out.println(firstLast6(new int[] {13, 6, 1, 2, 3}));
-}
+} // end main 
 
 } // end class
