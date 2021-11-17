79024b7dbc3e0d46dec2fe2899fb716f09b3bc51
jchen302023 <jchen30@stuy.edu>
2021-11-16 19:22:40 -0500

    wee

--- cb/array-1/firstLast6.java
+++ cb/array-1/firstLast6.java
@@ -7,19 +7,20 @@ firstLast6([13, 6, 1, 2, 3]) → false */
 
 public class firstLast6 {
 
-public static boolean firstLast6(int[] nums) {
-  int alength = nums.length;
-  if (nums[0] == 6 || nums[alength - 1] == 6) {
-    return true;
-  }
-  else {
-    return false;
-  }
-} // end method firstLast6
+  public static boolean firstLast6(int[] nums) {
+    int alength = nums.length;
+    if (nums[0] == 6 || nums[alength - 1] == 6) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  } // end method firstLast6
 
 public static void main (String[] args) {
   System.out.println(firstLast6(new int[] {1, 2, 6}));
   System.out.println(firstLast6(new int[] {6, 1, 2, 3}));
   System.out.println(firstLast6(new int[] {13, 6, 1, 2, 3}));
 }
+
 } // end class
