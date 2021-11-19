a9d8fb1b7e4ecf717511a3d244d96d474d2a777a
jchen302023 <jchen30@stuy.edu>
2021-11-19 13:14:49 -0500

    wee

---
+++ 39/Protagonist.java
@@ -0,0 +1,12 @@
+public class Protagonist {
+    public int attack() {
+
+    }
+    public boolean isAlive () {
+      int initialhealth = 100;
+
+    }
+    public String getName () {
+      
+    }
+}
