9892774f393c6beff2dc1f1170fb54f393170b03
jchen302023 <jchen30@stuy.edu>
2022-02-02 22:15:32 -0500

    cleannn

--- interCar_test/Car.java
+++
@@ -1,7 +0,0 @@
-// thanks p. serb :)
-
-public interface Car {
-  public int numseats();
-  public int horsepower();
-  public String color(); 
-}
--- relaxation/Output.java
+++
@@ -1,6 +0,0 @@
-class Output {
-  public static void main(String[] args) {
-    GrokStatic obj = new GrokStatic();
-    System.out.println(obj.func(12));
-  }
-}
