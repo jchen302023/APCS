07de63f023c2ea758d6a1c839c92cf483a51e948
jchen302023 <jchen30@stuy.edu>
2022-02-14 14:02:54 -0500

    wee

--- APCS_Sem1/cb/fibonacci.java
+++ APCS_Sem1/cb/fibonacci.java
@@ -7,5 +7,24 @@ fibonacci(2) → 1
 */
 
 public class fibonacci {
+  public static int fibonacci(int n) {
+    if (n == 0) {
+      return 0;
+    }
+    else if (n == 1) {
+      return 1;
+    }
+    else {
+      return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+  }
+  public static void main(String[] args) {
+    System.out.println(fibonacci(0));
+    System.out.println(fibonacci(1));
+    System.out.println(fibonacci(2));
+    System.out.println(fibonacci(3));
+    System.out.println(fibonacci(4));
 
-} 
+  }
+
+}
