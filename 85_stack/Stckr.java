fff6da400cdd0c8dc984dbe138d11bed701d0a49
Jack Chen <90912786+jchen302023@users.noreply.github.com>
2022-03-30 21:56:10 -0400

    sheesh

--- 85_stack/Stckr.java
+++ 85_stack/Stckr.java
@@ -11,15 +11,24 @@ public class Stckr
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // USAGE: un-comment exactly 1 assignment below
-    Stack<String> cakes = new ALStack<String>();
-    //Stack<String> cakes = new LLStack<String>();
-    
+    //Stack<Object> cakes = new ALStack<Object>();
+    Stack<Object> cakes = new LLStack<Object>();
+    System.out.println(cakes.isEmpty());
     cakes.push("blueberry");
     cakes.push("strawberry");
     cakes.push("raspberry");
+    cakes.push("vanilla");
+    cakes.push(100);
+    cakes.push(true);
     System.out.println(cakes.peekTop());
     System.out.println(cakes.pop());
+    System.out.println(cakes.isEmpty());
     System.out.println(cakes.pop()); 
+    System.out.println(cakes.pop()); 
+    System.out.println(cakes.pop()); 
+    System.out.println(cakes.pop()); 
+    System.out.println(cakes.isEmpty());
+    System.out.println("Does your code break");
     System.out.println(cakes.pop()); 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
