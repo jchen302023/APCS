8f43809bf3266a73208cd5c5081acbec7a87d54d
jchen302023 <jchen30@stuy.edu>
2022-05-13 14:02:43 -0400

    wee

---
+++ 99_alpq/ArrayPriorityQueue.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+
+public class ArrayPriorityQueue implements PriorityQueue {
+
+    ArrayList<Integer> mouse = new ArrayList<Integer>();
+
+
+  public void add( int x ) {
+    if (mouse.size() == 0) {
+      mouse.add(x);
+    }
+    if (x < mouse.get(mouse.size() - 1)) {
+      mouse.add(x);
+    }
+    else {
+      for (int i = 0; i < mouse.size(); i ++) {
+        if (x > mouse.get(i)) {
+          mouse.add(x, i);
+        }
+      }
+    }
+  }
+
+  public boolean isEmpty() {
+    return mouse.size() == 0;
+  }
+
+  public int peekMin() {
+    if (mouse.size() == 0) {
+      return 0;
+    }
+    return mouse.get(mouse.size()  - 1);
+  }
+
+  public int removeMin() {
+    return mouse.remove(mouse.size() - 1);
+  }
+
+}
